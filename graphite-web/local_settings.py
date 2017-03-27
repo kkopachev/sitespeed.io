1603f75508d8ee968b0d2eae521e6f7e579868b2
Konstantin Kopachev <tenzzor@gmail.com>
2017-03-27 12:44:11 -0700

    carbon + graphite-web + sitespeed-io + grafana config

---
+++ graphite-web/local_settings.py
@@ -0,0 +1,38 @@
+## Graphite local_settings.py
+# Edit this file to customize the default Graphite webapp settings
+#
+# Additional customizations to Django settings can be added to this file as well
+
+from os import getenv, urandom
+
+SECRET_KEY = getenv('SECRET_KEY', urandom(24).encode('hex'))
+
+if getenv("CLUSTER_SERVERS"):
+    CLUSTER_SERVERS = getenv("CLUSTER_SERVERS").split(',')
+
+DEBUG = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        'graphite': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+    },
+}
+
+try:
+    from graphite.custom_settings import *
+except ImportError:
+    pass
